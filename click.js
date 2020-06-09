563a3d4b39cc07671feb1fec2c3951c602fbb910
TinTin Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2020-06-09 13:11:21 +0700

    Delete click.js

--- click.js
+++
@@ -1,6 +0,0 @@
-function clickMain(){
-    cash = cash + mainPower * cashPower * prestigeMainPower * clickPower;
-    tokens = tokens + Math.random() * 0.0001 * mainPower * tokensPower * prestigeMainPower * clickPower;
-    document.getElementById("cash").innerHTML = cash;
-    document.getElementById("tokens").innerHTML = tokens;
-};
