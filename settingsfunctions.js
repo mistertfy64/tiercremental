fd6ba0850fa5347cf95ab728c5a80f824b18a0ff
TinTin Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2020-06-17 08:18:37 +0700

    v0.0.1 - Initial Alpha Release
    10 tiers of cash generators
    Cash/tokens doesnt save because im lazy

--- settingsfunctions.js
+++ settingsfunctions.js
@@ -19,6 +19,14 @@ function loadFromFile1(){
     var saveFileToLoad = JSON.parse(localStorage.getItem("saveFile1"));
     if (typeof saveFileToLoad.game.tier1CashGenerator !== "undefined") game.tier1CashGenerator = saveFileToLoad.game.tier1CashGenerator;
     if (typeof saveFileToLoad.game.tier2CashGenerator !== "undefined") game.tier2CashGenerator = saveFileToLoad.game.tier2CashGenerator;
+    if (typeof saveFileToLoad.game.tier3CashGenerator !== "undefined") game.tier3CashGenerator = saveFileToLoad.game.tier3CashGenerator;
+    if (typeof saveFileToLoad.game.tier4CashGenerator !== "undefined") game.tier4CashGenerator = saveFileToLoad.game.tier4CashGenerator;
+    if (typeof saveFileToLoad.game.tier5CashGenerator !== "undefined") game.tier5CashGenerator = saveFileToLoad.game.tier5CashGenerator;
+    if (typeof saveFileToLoad.game.tier6CashGenerator !== "undefined") game.tier6CashGenerator = saveFileToLoad.game.tier6CashGenerator;
+    if (typeof saveFileToLoad.game.tier7CashGenerator !== "undefined") game.tier7CashGenerator = saveFileToLoad.game.tier7CashGenerator;
+    if (typeof saveFileToLoad.game.tier8CashGenerator !== "undefined") game.tier8CashGenerator = saveFileToLoad.game.tier8CashGenerator;
+    if (typeof saveFileToLoad.game.tier9CashGenerator !== "undefined") game.tier9ashGenerator = saveFileToLoad.game.tier9CashGenerator;
+    if (typeof saveFileToLoad.game.tier10CashGenerator !== "undefined") game.tier10CashGenerator = saveFileToLoad.game.tier10CashGenerator;
 
     game.tier1CashGenerator.totalOwned = Decimal(game.tier1CashGenerator.totalOwned);
     game.tier1CashGenerator.totalPurchased = Decimal(game.tier1CashGenerator.totalPurchased);
@@ -33,5 +41,63 @@ function loadFromFile1(){
     game.tier2CashGenerator.productionMultiplier = Decimal(game.tier2CashGenerator.productionMultiplier);
     game.tier2CashGenerator.totalProduction = Decimal(game.tier2CashGenerator.totalProduction);
     game.tier2CashGenerator.productionPerGenerator = Decimal(game.tier2CashGenerator.productionPerGenerator);
+
+    game.tier3CashGenerator.totalOwned = Decimal(game.tier3CashGenerator.totalOwned);
+    game.tier3CashGenerator.totalPurchased = Decimal(game.tier3CashGenerator.totalPurchased);
+    game.tier3CashGenerator.totalGenerated = Decimal(game.tier3CashGenerator.totalGenerated);
+    game.tier3CashGenerator.productionMultiplier = Decimal(game.tier3CashGenerator.productionMultiplier);
+    game.tier3CashGenerator.totalProduction = Decimal(game.tier3CashGenerator.totalProduction);
+    game.tier3CashGenerator.productionPerGenerator = Decimal(game.tier3CashGenerator.productionPerGenerator);
+
+    game.tier4CashGenerator.totalOwned = Decimal(game.tier4CashGenerator.totalOwned);
+    game.tier4CashGenerator.totalPurchased = Decimal(game.tier4CashGenerator.totalPurchased);
+    game.tier4CashGenerator.totalGenerated = Decimal(game.tier4CashGenerator.totalGenerated);
+    game.tier4CashGenerator.productionMultiplier = Decimal(game.tier4CashGenerator.productionMultiplier);
+    game.tier4CashGenerator.totalProduction = Decimal(game.tier4CashGenerator.totalProduction);
+    game.tier4CashGenerator.productionPerGenerator = Decimal(game.tier4CashGenerator.productionPerGenerator);
+
+    game.tier5CashGenerator.totalOwned = Decimal(game.tier5CashGenerator.totalOwned);
+    game.tier5CashGenerator.totalPurchased = Decimal(game.tier5CashGenerator.totalPurchased);
+    game.tier5CashGenerator.totalGenerated = Decimal(game.tier5CashGenerator.totalGenerated);
+    game.tier5CashGenerator.productionMultiplier = Decimal(game.tier5CashGenerator.productionMultiplier);
+    game.tier5CashGenerator.totalProduction = Decimal(game.tier5CashGenerator.totalProduction);
+    game.tier5CashGenerator.productionPerGenerator = Decimal(game.tier5CashGenerator.productionPerGenerator);
+
+    game.tier6CashGenerator.totalOwned = Decimal(game.tier6CashGenerator.totalOwned);
+    game.tier6CashGenerator.totalPurchased = Decimal(game.tier6CashGenerator.totalPurchased);
+    game.tier6CashGenerator.totalGenerated = Decimal(game.tier6CashGenerator.totalGenerated);
+    game.tier6CashGenerator.productionMultiplier = Decimal(game.tier6CashGenerator.productionMultiplier);
+    game.tier6CashGenerator.totalProduction = Decimal(game.tier6CashGenerator.totalProduction);
+    game.tier6CashGenerator.productionPerGenerator = Decimal(game.tier6CashGenerator.productionPerGenerator);
+
+    game.tier7CashGenerator.totalOwned = Decimal(game.tier7CashGenerator.totalOwned);
+    game.tier7CashGenerator.totalPurchased = Decimal(game.tier7CashGenerator.totalPurchased);
+    game.tier7CashGenerator.totalGenerated = Decimal(game.tier7CashGenerator.totalGenerated);
+    game.tier7CashGenerator.productionMultiplier = Decimal(game.tier7CashGenerator.productionMultiplier);
+    game.tier7CashGenerator.totalProduction = Decimal(game.tier7CashGenerator.totalProduction);
+    game.tier7CashGenerator.productionPerGenerator = Decimal(game.tier7CashGenerator.productionPerGenerator);
+
+    game.tier8CashGenerator.totalOwned = Decimal(game.tier8CashGenerator.totalOwned);
+    game.tier8CashGenerator.totalPurchased = Decimal(game.tier8CashGenerator.totalPurchased);
+    game.tier8CashGenerator.totalGenerated = Decimal(game.tier8CashGenerator.totalGenerated);
+    game.tier8CashGenerator.productionMultiplier = Decimal(game.tier8CashGenerator.productionMultiplier);
+    game.tier8CashGenerator.totalProduction = Decimal(game.tier8CashGenerator.totalProduction);
+    game.tier8CashGenerator.productionPerGenerator = Decimal(game.tier8CashGenerator.productionPerGenerator);
+
+    game.tier9CashGenerator.totalOwned = Decimal(game.tier9CashGenerator.totalOwned);
+    game.tier9CashGenerator.totalPurchased = Decimal(game.tier9CashGenerator.totalPurchased);
+    game.tier9CashGenerator.totalGenerated = Decimal(game.tier9CashGenerator.totalGenerated);
+    game.tier9CashGenerator.productionMultiplier = Decimal(game.tier9CashGenerator.productionMultiplier);
+    game.tier9CashGenerator.totalProduction = Decimal(game.tier9CashGenerator.totalProduction);
+    game.tier9CashGenerator.productionPerGenerator = Decimal(game.tier9CashGenerator.productionPerGenerator);
+
+    game.tier10CashGenerator.totalOwned = Decimal(game.tier10CashGenerator.totalOwned);
+    game.tier10CashGenerator.totalPurchased = Decimal(game.tier10CashGenerator.totalPurchased);
+    game.tier10CashGenerator.totalGenerated = Decimal(game.tier10CashGenerator.totalGenerated);
+    game.tier10CashGenerator.productionMultiplier = Decimal(game.tier10CashGenerator.productionMultiplier);
+    game.tier10CashGenerator.totalProduction = Decimal(game.tier10CashGenerator.totalProduction);
+    game.tier10CashGenerator.productionPerGenerator = Decimal(game.tier10CashGenerator.productionPerGenerator);
+
+    
 }
 
--- updatefunctions.js
+++ updatefunctions.js
@@ -11,11 +11,67 @@ window.setInterval(function(){
     document.getElementById("tier-1-cash-generators-total-production").innerHTML = Decimal(game.tier1CashGenerator.totalProduction);
 
     document.getElementById("tier-2-cash-generators-owned").innerHTML = Decimal(game.tier2CashGenerator.totalOwned);
-    document.getElementById("tier-2-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier2CashGenerator.totalPurchased, game.tier2CashGenerator.costExponent)).plus(10000));
+    document.getElementById("tier-2-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier2CashGenerator.totalPurchased, game.tier2CashGenerator.costExponent)).plus(game.tier2CashGenerator.costAddend));
     document.getElementById("tier-2-cash-generators-purchased").innerHTML = Decimal(game.tier2CashGenerator.totalPurchased);
     document.getElementById("tier-2-cash-generators-generated").innerHTML = Decimal(game.tier2CashGenerator.totalGenerated);
     document.getElementById("tier-2-cash-generators-total-production").innerHTML = Decimal(game.tier2CashGenerator.totalProduction);
 
+    document.getElementById("tier-3-cash-generators-owned").innerHTML = Decimal(game.tier3CashGenerator.totalOwned);
+    document.getElementById("tier-3-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier3CashGenerator.totalPurchased, game.tier3CashGenerator.costExponent)).plus(game.tier3CashGenerator.costAddend));
+    document.getElementById("tier-3-cash-generators-purchased").innerHTML = Decimal(game.tier3CashGenerator.totalPurchased);
+    document.getElementById("tier-3-cash-generators-generated").innerHTML = Decimal(game.tier3CashGenerator.totalGenerated);
+    document.getElementById("tier-3-cash-generators-total-production").innerHTML = Decimal(game.tier3CashGenerator.totalProduction);
+
+    document.getElementById("tier-4-cash-generators-owned").innerHTML = Decimal(game.tier4CashGenerator.totalOwned);
+    document.getElementById("tier-4-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier4CashGenerator.totalPurchased, game.tier4CashGenerator.costExponent)).plus(game.tier4CashGenerator.costAddend));
+    document.getElementById("tier-4-cash-generators-purchased").innerHTML = Decimal(game.tier4CashGenerator.totalPurchased);
+    document.getElementById("tier-4-cash-generators-generated").innerHTML = Decimal(game.tier4CashGenerator.totalGenerated);
+    document.getElementById("tier-4-cash-generators-total-production").innerHTML = Decimal(game.tier4CashGenerator.totalProduction);
+
+    document.getElementById("tier-5-cash-generators-owned").innerHTML = Decimal(game.tier5CashGenerator.totalOwned);
+    document.getElementById("tier-5-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier5CashGenerator.totalPurchased, game.tier5CashGenerator.costExponent)).plus(game.tier5CashGenerator.costAddend));
+    document.getElementById("tier-5-cash-generators-purchased").innerHTML = Decimal(game.tier5CashGenerator.totalPurchased);
+    document.getElementById("tier-5-cash-generators-generated").innerHTML = Decimal(game.tier5CashGenerator.totalGenerated);
+    document.getElementById("tier-5-cash-generators-total-production").innerHTML = Decimal(game.tier5CashGenerator.totalProduction);
+
+    document.getElementById("tier-6-cash-generators-owned").innerHTML = Decimal(game.tier6CashGenerator.totalOwned);
+    document.getElementById("tier-6-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier6CashGenerator.totalPurchased, game.tier6CashGenerator.costExponent)).plus(game.tier6CashGenerator.costAddend));
+    document.getElementById("tier-6-cash-generators-purchased").innerHTML = Decimal(game.tier6CashGenerator.totalPurchased);
+    document.getElementById("tier-6-cash-generators-generated").innerHTML = Decimal(game.tier6CashGenerator.totalGenerated);
+    document.getElementById("tier-6-cash-generators-total-production").innerHTML = Decimal(game.tier6CashGenerator.totalProduction);
+
+    document.getElementById("tier-7-cash-generators-owned").innerHTML = Decimal(game.tier7CashGenerator.totalOwned);
+    document.getElementById("tier-7-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier7CashGenerator.totalPurchased, game.tier7CashGenerator.costExponent)).plus(game.tier7CashGenerator.costAddend));
+    document.getElementById("tier-7-cash-generators-purchased").innerHTML = Decimal(game.tier7CashGenerator.totalPurchased);
+    document.getElementById("tier-7-cash-generators-generated").innerHTML = Decimal(game.tier7CashGenerator.totalGenerated);
+    document.getElementById("tier-7-cash-generators-total-production").innerHTML = Decimal(game.tier7CashGenerator.totalProduction);
+
+    document.getElementById("tier-8-cash-generators-owned").innerHTML = Decimal(game.tier8CashGenerator.totalOwned);
+    document.getElementById("tier-8-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier8CashGenerator.totalPurchased, game.tier8CashGenerator.costExponent)).plus(game.tier8CashGenerator.costAddend));
+    document.getElementById("tier-8-cash-generators-purchased").innerHTML = Decimal(game.tier8CashGenerator.totalPurchased);
+    document.getElementById("tier-8-cash-generators-generated").innerHTML = Decimal(game.tier8CashGenerator.totalGenerated);
+    document.getElementById("tier-8-cash-generators-total-production").innerHTML = Decimal(game.tier8CashGenerator.totalProduction);
+
+    document.getElementById("tier-9-cash-generators-owned").innerHTML = Decimal(game.tier9CashGenerator.totalOwned);
+    document.getElementById("tier-9-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier9CashGenerator.totalPurchased, game.tier9CashGenerator.costExponent)).plus(game.tier9CashGenerator.costAddend));
+    document.getElementById("tier-9-cash-generators-purchased").innerHTML = Decimal(game.tier9CashGenerator.totalPurchased);
+    document.getElementById("tier-9-cash-generators-generated").innerHTML = Decimal(game.tier9CashGenerator.totalGenerated);
+    document.getElementById("tier-9-cash-generators-total-production").innerHTML = Decimal(game.tier9CashGenerator.totalProduction);
+
+    document.getElementById("tier-10-cash-generators-owned").innerHTML = Decimal(game.tier10CashGenerator.totalOwned);
+    document.getElementById("tier-10-cash-generator-cost").innerHTML = (Decimal.floor(Decimal.pow(game.tier10CashGenerator.totalPurchased, game.tier10CashGenerator.costExponent)).plus(game.tier10CashGenerator.costAddend));
+    document.getElementById("tier-10-cash-generators-purchased").innerHTML = Decimal(game.tier10CashGenerator.totalPurchased);
+    document.getElementById("tier-10-cash-generators-generated").innerHTML = Decimal(game.tier10CashGenerator.totalGenerated);
+    document.getElementById("tier-10-cash-generators-total-production").innerHTML = Decimal(game.tier10CashGenerator.totalProduction);
+
     game.tier1CashGenerator.totalProduction = Decimal(game.tier1CashGenerator.totalOwned);
     game.tier2CashGenerator.totalProduction = Decimal(game.tier2CashGenerator.totalOwned);
+    game.tier3CashGenerator.totalProduction = Decimal(game.tier3CashGenerator.totalOwned);
+    game.tier4CashGenerator.totalProduction = Decimal(game.tier4CashGenerator.totalOwned);
+    game.tier5CashGenerator.totalProduction = Decimal(game.tier5CashGenerator.totalOwned);
+    game.tier6CashGenerator.totalProduction = Decimal(game.tier6CashGenerator.totalOwned);
+    game.tier7CashGenerator.totalProduction = Decimal(game.tier7CashGenerator.totalOwned);
+    game.tier8CashGenerator.totalProduction = Decimal(game.tier8CashGenerator.totalOwned);
+    game.tier9CashGenerator.totalProduction = Decimal(game.tier9CashGenerator.totalOwned);
+    game.tier10CashGenerator.totalProduction = Decimal(game.tier10CashGenerator.totalOwned);
 }, 10)
